28c511bf8690b4367ef1b59b69d812ff707d59e9
ilya <marhoily@hotmail.com>
2014-09-18 23:27:05 +0300

    Generalize all tests

--- ChessKit.Logics.UnitTests/Bitboard.IsLegalPawnMove.cs
+++ ChessKit.Logics.UnitTests/Bitboard.IsLegalPawnMove.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using FluentAssertions;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace ChessKit.ChessLogic.UnitTests
 {
@@ -30,59 +28,41 @@ namespace ChessKit.ChessLogic.UnitTests
             _bitboard = new Bitboard();
         }
         [Test]
+        [TestCase(TestName="White pawn moves from e2 to e3 and e4 only")]
         public void IsLegalMove_For_White_Pawn_From_E2_To_Every_Other_Cell_Then_E3_E4_Should_Return_False()
         {
             _bitboard[Bitboard.E2] = BitType.WhitePawn;
 	
-            _bitboard.AssertLegalMoves(Bitboard.E2,Bitboard.E3, Bitboard.E4);
+            _bitboard.AssertLegalMoves(Bitboard.E2, Bitboard.E3, Bitboard.E4);
         }
 
+        [Test]
+        [TestCase(TestName="White pawn doesn't move from e2 if blocked on e3")]
+        public void IsLegalMove_For_White_Pawn_From_E2_To_Every_Other_Cell_Then__Should_Return_False()
+        {
+            _bitboard[Bitboard.E2] = BitType.WhitePawn;
+            _bitboard[Bitboard.E3] = BitType.WhitePawn;
 	
+            _bitboard.AssertLegalMoves(Bitboard.E2);
+        }
 
         [Test]
-        public void IsLegalMove_For_White_Pawn_From_E3_To_Every_Other_Cell_Then_E4_Should_Return_False()
-        {
-            _bitboard[Bitboard.E3] = BitType.WhitePawn;
-            _bitboard.AssertLegalMoves(Bitboard.E3, Bitboard.E4);
-        }
-        [Test]
-        public void IsLegalMove_For_White_Pawn_When_DoublePush_From_Third_Row_Should_Return_False()
-        {
-            _bitboard[Bitboard.E3] = BitType.WhitePawn;
-            _bitboard.IsLegalMove(Bitboard.E3, Bitboard.E5).Should().BeFalse();
-        }
-        [Test]
-        public void IsLegalMove_For_White_Pawn_From_Second_To_Fourth_Row_When_Blocked_On_Fourth_Row_Should_Return_False()
+        [TestCase(TestName="White pawn only moves from e2 to e3 if blocked on e4")]
+        public void IsLegalMove_For_White_Pawn_From_E2_To_Every_Other_Cell_Then_E3_Should_Return_False()
         {
             _bitboard[Bitboard.E2] = BitType.WhitePawn;
             _bitboard[Bitboard.E4] = BitType.WhitePawn;
-            _bitboard.IsLegalMove(Bitboard.E2, Bitboard.E4).Should().BeFalse();
+	
+            _bitboard.AssertLegalMoves(Bitboard.E2, Bitboard.E3);
         }
+
         [Test]
-        public void IsLegalMove_For_White_Pawn_From_Second_To_Fourth_Row_When_Blocked_On_Third_Row_Should_Return_False()
+        [TestCase(TestName="White pawn only moves from e3 to e4")]
+        public void IsLegalMove_For_White_Pawn_From_E3_To_Every_Other_Cell_Then_E4_Should_Return_False()
         {
-            _bitboard[Bitboard.E2] = BitType.WhitePawn;
             _bitboard[Bitboard.E3] = BitType.WhitePawn;
-            _bitboard.IsLegalMove(Bitboard.E2, Bitboard.E4).Should().BeFalse();
+	
+            _bitboard.AssertLegalMoves(Bitboard.E3, Bitboard.E4);
         }
-        [Test]
-        public void IsLegalMove_For_White_Pawn_From_Second_To_Fourth_Row_Should_Return_True()
-        {
-            _bitboard[Bitboard.E2] = BitType.WhitePawn;
-            _bitboard.IsLegalMove(Bitboard.E2, Bitboard.E4).Should().BeTrue();
-        }
-        [Test]
-        public void IsLegalMove_For_White_Pawn_From_Second_To_Third_Row_Should_Return_True()
-        {
-            _bitboard[Bitboard.E2] = BitType.WhitePawn;
-            _bitboard.IsLegalMove(Bitboard.E2, Bitboard.E3).Should().BeTrue();
-        }
-        [Test]
-        public void IsLegalMove_For_White_Pawn_From_Second_To_Fifth_Row_Should_Return_False()
-        {
-            _bitboard[Bitboard.E2] = BitType.WhitePawn;
-            _bitboard.IsLegalMove(Bitboard.E2, 36).Should().BeFalse();
-        }
-		
     }
 }
