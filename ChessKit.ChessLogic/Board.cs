22e620d90f6c1b193dca7245e5a4d076ed6f9d07
ilya <marhoily@gmail.com>
2015-06-21 18:05:20 +0300

    refactor

--- ChessKit.ChessLogic/Board.cs
+++ ChessKit.ChessLogic/Board.cs
@@ -4,214 +4,210 @@ using System.Diagnostics.CodeAnalysis;
 namespace ChessKit.ChessLogic
 {
     public sealed partial class Board
-	{
-		private GameState _gameState;
+    {
+        private GameState _gameState;
 
-		/// <summary>The side which is moving next (white or black)</summary>
-		public PieceColor SideOnMove { get; private set; }
-		/// <summary>Gets the file at which en passant move is available</summary>
-		[SuppressMessage("Microsoft.Naming",
-		  "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "En",
-		  Justification = "'En-passant' is borrowed from french I guess")]
-		public int? EnPassantFile { get; set; }
+        /// <summary>The side which is moving next (white or black)</summary>
+        public PieceColor SideOnMove { get; private set; }
+        /// <summary>Gets the file at which en passant move is available</summary>
+        [SuppressMessage("Microsoft.Naming",
+          "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "En",
+          Justification = "'En-passant' is borrowed from french I guess")]
+        public int? EnPassantFile { get; set; }
 
-		/// <summary>Gets sides players can castle to</summary>
-		private CastlingAvailability _castlingAvailability;
-		/// <summary>This is the number of halfmoves since the last pawn advance or capture. </summary>
-		/// <remarks>This is used to determine if a draw can be claimed under the fifty-move rule.</remarks>
-		public int HalfMoveClock { get; set; }
-		/// <summary>The number of the full move. It starts at 1, and is incremented after Black's move</summary>
-		public int MoveNumber { get; private set; }
+        /// <summary>Gets sides players can castle to</summary>
+        private CastlingAvailability _castlingAvailability;
+        /// <summary>This is the number of halfmoves since the last pawn advance or capture. </summary>
+        /// <remarks>This is used to determine if a draw can be claimed under the fifty-move rule.</remarks>
+        public int HalfMoveClock { get; set; }
+        /// <summary>The number of the full move. It starts at 1, and is incremented after Black's move</summary>
+        public int MoveNumber { get; private set; }
 
-		public Board Previous { get; private set; }
-		public Move PreviousMove { get; }
+        public Board Previous { get; private set; }
+        public Move PreviousMove { get; }
 
-		public bool IsCheck => _gameState == GameState.Check;
-        public bool IsMate => _gameState == GameState.WhiteWin || _gameState == GameState.BlackWin;
+        public bool IsCheck => _gameState == GameState.Check;
+        public bool IsMate => _gameState == GameState.Mate;
 
         #region ' MakeMove '
 
-		[SuppressMessage("Microsoft.Design", "CA1002:DoNotExposeGenericLists"), SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate",
-		  Justification = "Takes considerable amount of time")]
-		public List<Move> GetLegalMoves()
-		{
-			// BUG: Actually creates boards, but only returns moves!
-			var res = new List<Move>(50);
-			var sideOnMove = SideOnMove;
-			for (var moveFrom = 0; moveFrom < 64; moveFrom++)
-			{
-				var moveFromSq = moveFrom + (moveFrom & ~7);
-				var piece = this[moveFromSq];
-				if (piece == 0) continue;
-				if (Piece.UnpackColor(piece) != sideOnMove) continue;
-				GenerateMoves(piece, moveFromSq, EnPassantFile, _castlingAvailability, res);
-			}
-			return res;
-		}
-		public List<Move> GetLegalMoves(Position from)
-		{
-			return GetLegalMoves((int)@from);
-		}
-		public List<Move> GetLegalMoves(int moveFrom)
-		{
-			var piece = this[moveFrom];
-			if (piece == CompactPiece.EmptyCell) return new List<Move>();
-			if (Piece.UnpackColor(piece) != SideOnMove) return new List<Move>();
-			var res = new List<Move>(28);
-			GenerateMoves(piece, moveFrom, EnPassantFile, _castlingAvailability, res);
-			return res;
-		}
+        [SuppressMessage("Microsoft.Design", "CA1002:DoNotExposeGenericLists"), SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate",
+          Justification = "Takes considerable amount of time")]
+        public List<Move> GetLegalMoves()
+        {
+            // BUG: Actually creates boards, but only returns moves!
+            var res = new List<Move>(50);
+            var sideOnMove = SideOnMove;
+            for (var moveFrom = 0; moveFrom < 64; moveFrom++)
+            {
+                var moveFromSq = moveFrom + (moveFrom & ~7);
+                var piece = this[moveFromSq];
+                if (piece == 0) continue;
+                if (Piece.UnpackColor(piece) != sideOnMove) continue;
+                GenerateMoves(piece, moveFromSq, EnPassantFile, _castlingAvailability, res);
+            }
+            return res;
+        }
+        public List<Move> GetLegalMoves(Position from)
+        {
+            return GetLegalMoves((int)@from);
+        }
+        public List<Move> GetLegalMoves(int moveFrom)
+        {
+            var piece = this[moveFrom];
+            if (piece == CompactPiece.EmptyCell) return new List<Move>();
+            if (Piece.UnpackColor(piece) != SideOnMove) return new List<Move>();
+            var res = new List<Move>(28);
+            GenerateMoves(piece, moveFrom, EnPassantFile, _castlingAvailability, res);
+            return res;
+        }
 
 
-		private void SetStatus()
-		{
-			if (!IsUnderCheck(SideOnMove))
-			{
-				return;
-			}
-			if (GetLegalMoves().Count > 0)
-				_gameState = GameState.Check;
-			else _gameState = SideOnMove == PieceColor.White ?
-			  GameState.BlackWin : GameState.WhiteWin;
-		}
-		public Board MakeMove(Move move)
-		{
-			return new Board(this, move);
-		}
+        private void SetStatus()
+        {
+            if (!IsUnderCheck(SideOnMove)) return;
+            _gameState = GetLegalMoves().Count > 0
+                ? GameState.Check : GameState.Mate;
+        }
 
-		private Board(Board src, Move move)
-			: this(src)
-		{
-			PreviousMove = move;
-			Previous = src;
+        public Board MakeMove(Move move)
+        {
+            return new Board(this, move);
+        }
 
-			// Piece in the from cell?
-			var moveFrom = (int)move.From;
-			var piece = src[moveFrom];
-			if (piece == CompactPiece.EmptyCell)
-			{
-				PreviousMove.Annotations = MoveAnnotations.EmptyCell;
-				return;
-			}
+        private Board(Board src, Move move)
+            : this(src)
+        {
+            PreviousMove = move;
+            Previous = src;
 
-			// Side to move?
-			var color = Piece.UnpackColor(piece);
-			if (color != src.SideOnMove)
-			{
-				PreviousMove.Annotations = (MoveAnnotations)piece | MoveAnnotations.WrongSideToMove;
-				return;
-			}
+            // Piece in the from cell?
+            var moveFrom = (int)move.From;
+            var piece = src[moveFrom];
+            if (piece == CompactPiece.EmptyCell)
+            {
+                PreviousMove.Annotations = MoveAnnotations.EmptyCell;
+                return;
+            }
 
-			// Move to occupied cell?
-			var moveTo = (int)move.To;
-			var toPiece = src[moveTo];
-			if (toPiece != CompactPiece.EmptyCell && Piece.UnpackColor(toPiece) == color)
-			{
-				PreviousMove.Annotations = (MoveAnnotations)piece | MoveAnnotations.ToOccupiedCell;
-				return;
-			}
-			PreviousMove.Annotations = src.ValidateMove(piece,
-			  moveFrom, moveTo, toPiece, src._castlingAvailability);
-			if (toPiece != CompactPiece.EmptyCell) PreviousMove.Annotations |= MoveAnnotations.Capture;
-			SetupBoard(src, piece, moveFrom, moveTo, move.ProposedPromotion, color);
-			if ((PreviousMove.Annotations & MoveAnnotations.AllErrors) != 0) return;
-			if (IsUnderCheck(SideOnMove))
-			{
-				PreviousMove.Annotations |= MoveAnnotations.Check;
-				_gameState = GameState.Check;
-			}
-			PreviousMove.Annotations |= MoveAnnotations.TestedForConsequences;
-		}
-		private void SetupBoard(Board src, CompactPiece piece,
-		  int moveFrom, int moveTo, PieceType proposedPromotion,
-		  PieceColor color)
-		{
-			if ((PreviousMove.Annotations & MoveAnnotations.AllErrors) != 0) return;
-			if ((PreviousMove.Annotations & MoveAnnotations.EnPassant) != 0)
-			{
-				if (src.EnPassantFile != moveTo % 16)
-				{
-					PreviousMove.Annotations |= MoveAnnotations.HasNoEnPassant;
-					return;
-				}
-			}
-			else if ((PreviousMove.Annotations & MoveAnnotations.Promotion) != 0)
-			{
-				piece = Piece.Pack(proposedPromotion, color);
-			}
+            // Side to move?
+            var color = Piece.UnpackColor(piece);
+            if (color != src.SideOnMove)
+            {
+                PreviousMove.Annotations = (MoveAnnotations)piece | MoveAnnotations.WrongSideToMove;
+                return;
+            }
 
-			this[moveTo] = piece;
-			_cells[moveFrom] = 0;
+            // Move to occupied cell?
+            var moveTo = (int)move.To;
+            var toPiece = src[moveTo];
+            if (toPiece != CompactPiece.EmptyCell && Piece.UnpackColor(toPiece) == color)
+            {
+                PreviousMove.Annotations = (MoveAnnotations)piece | MoveAnnotations.ToOccupiedCell;
+                return;
+            }
+            PreviousMove.Annotations = src.ValidateMove(piece,
+              moveFrom, moveTo, toPiece, src._castlingAvailability);
+            if (toPiece != CompactPiece.EmptyCell) PreviousMove.Annotations |= MoveAnnotations.Capture;
+            SetupBoard(src, piece, moveFrom, moveTo, move.ProposedPromotion, color);
+            if ((PreviousMove.Annotations & MoveAnnotations.AllErrors) != 0) return;
+            if (IsUnderCheck(SideOnMove))
+            {
+                PreviousMove.Annotations |= MoveAnnotations.Check;
+                _gameState = GameState.Check;
+            }
+            PreviousMove.Annotations |= MoveAnnotations.TestedForConsequences;
+        }
+        private void SetupBoard(Board src, CompactPiece piece,
+          int moveFrom, int moveTo, PieceType proposedPromotion,
+          PieceColor color)
+        {
+            if ((PreviousMove.Annotations & MoveAnnotations.AllErrors) != 0) return;
+            if ((PreviousMove.Annotations & MoveAnnotations.EnPassant) != 0)
+            {
+                if (src.EnPassantFile != moveTo % 16)
+                {
+                    PreviousMove.Annotations |= MoveAnnotations.HasNoEnPassant;
+                    return;
+                }
+            }
+            else if ((PreviousMove.Annotations & MoveAnnotations.Promotion) != 0)
+            {
+                piece = Piece.Pack(proposedPromotion, color);
+            }
 
-			if ((PreviousMove.Annotations & (MoveAnnotations.PawnDoublePush | MoveAnnotations.EnPassant | MoveAnnotations.Castling)) != 0)
-			{
-				if ((PreviousMove.Annotations & MoveAnnotations.PawnDoublePush) != 0)
-				{
-					EnPassantFile = moveFrom % 16;
-				}
-				else if ((PreviousMove.Annotations & MoveAnnotations.EnPassant) != 0)
-				{
-					_cells[moveTo + (color == PieceColor.White ? -16 : +16)] = 0;
-				}
-				else if (PreviousMove.Annotations == (MoveAnnotations.Castling | MoveAnnotations.WhiteKingsideCastling)) // TODO: Move it up?
-				{
-					_cells[S.H1] = (byte)CompactPiece.EmptyCell;
-					_cells[S.F1] = (byte)CompactPiece.WhiteRook;
-				}
-				else if (PreviousMove.Annotations == (MoveAnnotations.Castling | MoveAnnotations.WhiteQueensideCastling))
-				{
-					_cells[S.A1] = (byte)CompactPiece.EmptyCell;
-					_cells[S.D1] = (byte)CompactPiece.WhiteRook;
-				}
-				else if (PreviousMove.Annotations == (MoveAnnotations.Castling | MoveAnnotations.BlackKingsideCastling))
-				{
-					_cells[S.H8] = (byte)CompactPiece.EmptyCell;
-					_cells[S.F8] = (byte)CompactPiece.BlackRook;
-				}
-				else if (PreviousMove.Annotations == (MoveAnnotations.Castling | MoveAnnotations.BlackQueensideCastling))
-				{
-					_cells[S.A8] = (byte)CompactPiece.EmptyCell;
-					_cells[S.D8] = (byte)CompactPiece.BlackRook;
-				}
-			}
-			if (IsUnderCheck(src.SideOnMove))
-			{
-				PreviousMove.Annotations |= MoveAnnotations.MoveToCheck;
-			}
-			_castlingAvailability = src._castlingAvailability
-			  & ~KilledAvailability(moveTo)
-			  & ~KilledAvailability(moveFrom);
+            this[moveTo] = piece;
+            _cells[moveFrom] = 0;
 
-			SideOnMove = color.Invert();
+            if ((PreviousMove.Annotations & (MoveAnnotations.PawnDoublePush | MoveAnnotations.EnPassant | MoveAnnotations.Castling)) != 0)
+            {
+                if ((PreviousMove.Annotations & MoveAnnotations.PawnDoublePush) != 0)
+                {
+                    EnPassantFile = moveFrom % 16;
+                }
+                else if ((PreviousMove.Annotations & MoveAnnotations.EnPassant) != 0)
+                {
+                    _cells[moveTo + (color == PieceColor.White ? -16 : +16)] = 0;
+                }
+                else if (PreviousMove.Annotations == (MoveAnnotations.Castling | MoveAnnotations.WhiteKingsideCastling)) // TODO: Move it up?
+                {
+                    _cells[S.H1] = (byte)CompactPiece.EmptyCell;
+                    _cells[S.F1] = (byte)CompactPiece.WhiteRook;
+                }
+                else if (PreviousMove.Annotations == (MoveAnnotations.Castling | MoveAnnotations.WhiteQueensideCastling))
+                {
+                    _cells[S.A1] = (byte)CompactPiece.EmptyCell;
+                    _cells[S.D1] = (byte)CompactPiece.WhiteRook;
+                }
+                else if (PreviousMove.Annotations == (MoveAnnotations.Castling | MoveAnnotations.BlackKingsideCastling))
+                {
+                    _cells[S.H8] = (byte)CompactPiece.EmptyCell;
+                    _cells[S.F8] = (byte)CompactPiece.BlackRook;
+                }
+                else if (PreviousMove.Annotations == (MoveAnnotations.Castling | MoveAnnotations.BlackQueensideCastling))
+                {
+                    _cells[S.A8] = (byte)CompactPiece.EmptyCell;
+                    _cells[S.D8] = (byte)CompactPiece.BlackRook;
+                }
+            }
+            if (IsUnderCheck(src.SideOnMove))
+            {
+                PreviousMove.Annotations |= MoveAnnotations.MoveToCheck;
+            }
+            _castlingAvailability = src._castlingAvailability
+              & ~KilledAvailability(moveTo)
+              & ~KilledAvailability(moveFrom);
 
-			HalfMoveClock =
-			  (PreviousMove.Annotations & (MoveAnnotations.Capture | MoveAnnotations.Pawn)) != 0
-			  ? 0 : src.HalfMoveClock + 1;
+            SideOnMove = color.Invert();
 
-			MoveNumber = src.MoveNumber + (color == PieceColor.Black ? 1 : 0);
-		}
+            HalfMoveClock =
+              (PreviousMove.Annotations & (MoveAnnotations.Capture | MoveAnnotations.Pawn)) != 0
+              ? 0 : src.HalfMoveClock + 1;
 
-		private static CastlingAvailability KilledAvailability(int pos)
-		{
-			switch (pos)
-			{
-				case S.A1: return CastlingAvailability.WhiteQueen;
-				case S.E1: return CastlingAvailability.White;
-				case S.H1: return CastlingAvailability.WhiteKing;
-				case S.A8: return CastlingAvailability.BlackQueen;
-				case S.E8: return CastlingAvailability.Black;
-				case S.H8: return CastlingAvailability.BlackKing;
-				default: return CastlingAvailability.None;
-			}
-		}
-		#endregion
+            MoveNumber = src.MoveNumber + (color == PieceColor.Black ? 1 : 0);
+        }
+
+        private static CastlingAvailability KilledAvailability(int pos)
+        {
+            switch (pos)
+            {
+                case S.A1: return CastlingAvailability.WhiteQueen;
+                case S.E1: return CastlingAvailability.White;
+                case S.H1: return CastlingAvailability.WhiteKing;
+                case S.A8: return CastlingAvailability.BlackQueen;
+                case S.E8: return CastlingAvailability.Black;
+                case S.H8: return CastlingAvailability.BlackKing;
+                default: return CastlingAvailability.None;
+            }
+        }
+        #endregion
 
 
-		[SuppressMessage("Microsoft.Design", "CA1043:UseIntegralOrStringArgumentForIndexers",
-		  Justification = "It just seems right to use Position as a natural indexer")]
-		public Piece this[Position index] => Piece.Unpack((CompactPiece)_cells[(int)index]);
+        [SuppressMessage("Microsoft.Design", "CA1043:UseIntegralOrStringArgumentForIndexers",
+          Justification = "It just seems right to use Position as a natural indexer")]
+        public Piece this[Position index] => Piece.Unpack((CompactPiece)_cells[(int)index]);
 
         public Piece this[string index] => this[Position.Parse(index)];
-	}
+    }
 }
 
--- ChessKit.ChessLogic/GameState.cs
+++ ChessKit.ChessLogic/GameState.cs
@@ -4,10 +4,8 @@ namespace ChessKit.ChessLogic
   {
     /// <summary>No flags set: game is not over yet</summary>
     None,
-    /// <summary>Black won (either by mating white king, or white resign)</summary>
-    BlackWin,
-    /// <summary>White won (either by mating black king, or black resign)</summary>
-    WhiteWin,
+    /// <summary></summary>
+    Mate,
     /// <summary>Game ended with draw (either by repetition, impasse or an agreement)</summary>
     Draw,
     /// <summary>Game ended with draw by repetition</summary>
