55979a47dd2809927e214e1e58eabd157220bb4e
ilya <marhoily@gmail.com>
2015-06-21 16:06:42 +0300

    refactor

--- ChessKit.Logics/Properties/AssemblyInfo.cs
+++ ChessKit.Logics/Properties/AssemblyInfo.cs
@@ -1,17 +1,16 @@
 ﻿using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChessKit.Logics")]
+[assembly: AssemblyTitle("ChessKit.ChessLogic")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ChessKit.Logics")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Marhoily 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
