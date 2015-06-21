eb22eda173d911f49d806a7c9ef67727ac4eace2
ilya <marhoily@gmail.com>
2015-06-21 21:18:00 +0300

    refactor

--- ChessKit.ChessLogic/PieceType.cs
+++ ChessKit.ChessLogic/PieceType.cs
@@ -2,16 +2,16 @@
 
 namespace ChessKit.ChessLogic
 {
-  [SuppressMessage("Microsoft.Design", "CA1027:MarkEnumsWithFlags",
-    Justification = "It really is not flags")]
-  public enum PieceType
-  {
-    None,
-    Pawn   = MoveAnnotations.Pawn,
-    Bishop = MoveAnnotations.Bishop ,
-    Knight = MoveAnnotations.Knight ,
-    Rook   = MoveAnnotations.Rook   ,
-    Queen  = MoveAnnotations.Queen  ,
-    King   = MoveAnnotations.King   ,
-  }
+    [SuppressMessage("Microsoft.Design", "CA1027:MarkEnumsWithFlags",
+        Justification = "It really is not flags")]
+    public enum PieceType
+    {
+        None,
+        Pawn = MoveAnnotations.Pawn,
+        Bishop = MoveAnnotations.Bishop,
+        Knight = MoveAnnotations.Knight,
+        Rook = MoveAnnotations.Rook,
+        Queen = MoveAnnotations.Queen,
+        King = MoveAnnotations.King
+    }
 }
