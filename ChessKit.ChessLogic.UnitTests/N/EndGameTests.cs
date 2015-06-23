faa3c279ed163bce10581b55f86cfb534ed3e314
ilya <marhoily@gmail.com>
2015-06-23 11:50:22 +0300

    Mate -> State

--- ChessKit.ChessLogic.UnitTests/N/EndGameTests.cs
+++ ChessKit.ChessLogic.UnitTests/N/EndGameTests.cs
@@ -9,6 +9,9 @@ namespace ChessKit.ChessLogic.UnitTests.N
         [Fact]
         public void GivesCheck() => CheckProperties(
             "8/2Rk4/1q4BP/8/8/6K1/8/8 b - - 24 119", "b6-c7", "Check");
+        [Fact]
+        public void GivesMate() => CheckProperties(
+            "2K5/8/2k4r/8/8/8/8/8 b - - 0 9", "h6-h8", "Mate");
 
         private static void CheckProperties(string fen, string move, string expectedProperties)
         {
--- ChessKit.ChessLogic/N/EndGame.cs
+++ ChessKit.ChessLogic/N/EndGame.cs
@@ -12,6 +12,7 @@ namespace ChessKit.ChessLogic.N
             var piece = legalMove.Piece;
             var obs = legalMove.Annotations;
             var color = prev.Core.ActiveColor;
+            var tempPosition = new Position(core, 0, 0, 0, legalMove);
 
             var newHalfMoveClock = 
                 piece == PieceType.Pawn || (obs & Capture) != 0
@@ -21,9 +22,12 @@ namespace ChessKit.ChessLogic.N
                 prev.FullMoveNumber + (color == Color.Black ? 1 : 0);
 
             var isCheck = core.IsInCheck(core.ActiveColor);
+            var noMoves = tempPosition.GetAllLegalMoves().Count == 0;
 
-            var newState = GameStates.None;
-            if (isCheck) newState |= GameStates.Check;
+            var newState = default(GameStates);
+            if (isCheck && noMoves) newState |= GameStates.Mate;
+            if (isCheck && !noMoves) newState |= GameStates.Check;
+            if (!isCheck && noMoves) newState |= GameStates.Stalemate;
 
             return new Position(core, newHalfMoveClock, 
                 newMoveNumber, newState, legalMove);
