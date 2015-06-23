fccf9f75f1eadd13252bd870b8e0797998797a9a
ilya <marhoily@gmail.com>
2015-06-23 11:34:27 +0300

    + EndGameTests

---
+++ ChessKit.ChessLogic.UnitTests/N/EndGameTests.cs
@@ -0,0 +1,21 @@
+﻿using ChessKit.ChessLogic.N;
+using FluentAssertions;
+using Xunit;
+
+namespace ChessKit.ChessLogic.UnitTests.N
+{
+    public sealed class EndGameTests
+    {
+        [Fact]
+        public void GivesCheck() => CheckAnnotations(
+            "8/2Rk4/1q4BP/8/8/6K1/8/8 b - - 24 119", "b6-c7", "Check");
+
+        private static void CheckAnnotations(string fen, string move, string expectedAnnotations)
+        {
+            fen.ParseFen().FromBoard()
+                .ValidateLegal(MoveR.Parse(move))
+                .Annotations.ToString()
+                .Should().Be(expectedAnnotations);
+        }
+    }
+}
